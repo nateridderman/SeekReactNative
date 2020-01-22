413d63eba6332dde81c1121430111ae73f139d02
Amanda Bullington <albullington@gmail.com>
2020-01-22 10:34:01 -0800

    Updates to fastlane screenshots

--- ios/SeekUITests/SeekUITests.swift
+++ ios/SeekUITests/SeekUITests.swift
@@ -12,6 +12,10 @@ class SeekUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+      
+        let app = XCUIApplication()
+        setupSnapshot(app)
+        app.launch()
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
